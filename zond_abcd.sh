60a094a5c545ea6c74ef6f9b1f24a0b59b7e7801
Fil_98 <45718052+Fil98@users.noreply.github.com>
2021-07-06 10:26:44 +0300

    Update

--- zond_abcd.sh
+++ zond_abcd.sh
@@ -1,64 +1,57 @@
 #!/bin/bash
-mypath=/usr/meteo/konst
-cmb="cmb -v"
-
-cd /usr/meteo/konst 
-
-$cmb USRS13RUMS >> zond_A 
-$cmb UKRS13RUMS >> zond_B
-
-cat zond_B >> zond_A
-
-$cmb USRA15RUAA >> zond_C 
-$cmb UKRA15RUAA >> zond_D 
-
-cat zond_D >> zond_C
-cat zond_C >> zond_A
-
-$cmb USRA16RUNW >> zond_E   
-$cmb UKRA16RUNW >> zond_F 
-
-cat zond_F >> zond_E
-cat zond_E >> zond_A
-
-$cmb ULRS13RUMS >> zond_G
-$cmb UERS13RUMS >> zond_H    
-
-cat zond_H >> zond_G  
-
-$cmb ULRA15RUAA >> zond_I  
-$cmb UERA15RUAA >> zond_K  
-
-cat zond_K >> zond_I
-cat zond_I >> zond_G
-
-$cmb ULRA16RUNW >> zond_L
-$cmb UERA16RUNW >> zond_M 
-
-cat zond_M >> zond_L
-cat zond_L >> zond_G
-cat zond_G >> zond_A
-
+$date=`date "+%d%m%y"`
+path1=/home/meteo/data/printed/$date
+#cd $path
+path=/home/meteo/zond/
+zond=/home/meteo/zond/zond_R
+#cd $path1
+cd $path1
+grep --null -l -i "USRS13 RUMS" * | xargs -0 cat > $path/zond_A 
+grep --null -l -i "UKRS13 RUMS" * | xargs -0 cat > $path/zond_B
+cat $path/zond_B >> $path/zond_A 
+grep --null -l -i "USRA15 RUAA" * | xargs -0 cat > $path/zond_C 
+grep --null -l -i "UKRA15 RUAA" * | xargs -0 cat > $path/zond_D
+cat $path/zond_D >> $path/zond_C
+cat $path/zond_C >> $path/zond_A
+grep --null -l -i "USRA16 RUNW" * | xargs -0 cat > $path/zond_E
+grep --null -l -i "UKRA16 RUNW" * | xargs -0 cat > $path/zond_F
+cat $path/zond_F >> $path/zond_E
+cat $path/zond_E >> $path/zond_A
+grep --null -l -i "ULRS13 RUMS" * | xargs -0 cat > $path/zond_G
+grep --null -l -i "UERS13 RUMS" * | xargs -0 cat > $path/zond_H
+cat $path/zond_H >> $path/zond_G
+grep --null -l -i "ULRA15 RUAA" * | xargs -0 cat > $path/zond_I
+grep --null -l -i "UERA15 RUAA" * | xargs -0 cat > $path/zond_K
+cat $path/zond_K >> $path/zond_I
+cat $path/zond_I >> $path/zond_G
+grep --null -l -i "ULRA16 RUNW" * | xargs -0 cat > $path/zond_L
+grep --null -l -i "UERA16 RUNW" * | xargs -0 cat > $path/zond_M
+cat $path/zond_M >> $path/zond_L
+cat $path/zond_L >> $path/zond_G
+cat $path/zond_G >> $path/zond_A
+cd $path
 tr -d "[\001\003\015]" < zond_A > zond_N
 grep -v '[*U]' zond_N > zond_O
 grep '[A-Z0-9^0-9]' zond_O > zond_P
 grep -v '^...$' zond_P > zond_R
-#mail -s `date +%d%m` sbor@arh.mecom.ru < zond_R
-#mail -s `date +%d%m` aero@arh.mecom.ru < zond_R
-#cp zond_R /usr/meteo/aero/`date +%d`
-rm $mypath/zond_A
-rm $mypath/zond_B
-rm $mypath/zond_C
-rm $mypath/zond_D
-rm $mypath/zond_E
-rm $mypath/zond_F
-rm $mypath/zond_G
-rm $mypath/zond_H
-rm $mypath/zond_I
-rm $mypath/zond_K
-rm $mypath/zond_L:
-rm $mypath/zond_M
-rm $mypath/zond_N
-rm $mypath/zond_O
-rm $mypath/zond_P
-rm $mypath/zond_R
+#sed -i '/NNNN/ d' $zond
+#sed -i '/USRS13/ d' $zond
+#sed -i '/UKRS13/ d' $zond
+#sed -i '/USRA15/ d' $zond
+#sed -i '/UKRA15/ d' $zond
+#sed -i '/USRA16/ d' $zond
+#sed -i '/ULRS13/ d' $zond
+#sed -i '/UERS13/ d' $zond
+#sed -i '/USRA15/ d' $zond
+#sed -i '/ULRA15/ d' $zond
+#sed -i '/UERA15/ d' $zond
+#sed -i '/ULRA16/ d' $zond
+#sed -i '/UERA16/ d' $zond
+sed -i '/NNNNZCZC/ d' $zond
+sed -i '/ZCZC/ d' $zond
+sed -i '/NNNN/ d' $zond
+mail -s `date +%d%m` aero@arh.mecom.ru < zond_R
+mail -s `date +%d%m` sbor@arh.mecom.ru < zond_R
+cp zond_R /usr/meteo/aero/`date +%d`
+#sed -i '/^$/d' $zond
+rm $path/zond_*
